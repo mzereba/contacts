7f51756ca9cb759847b7a39e82476daf1a7b600d
mzereba <mzereba@mzereba-PC>
2015-02-06 00:01:15 +0300

    Added contacts filtering feature, bug fixing

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ module.controller('ContactController', function ($scope, $http, $sce) {
     $scope.modalTitle = '';
     
     $scope.user = '';	// 'https://mzereba.rww.io/profile/card#me'
-    $scope.path1 = '';	//	'http://mzereba.rww.io/storage/contacts/';
-    $scope.path = 'http://essam.crosscloud.qcri.org/storage/contacts/';
+    $scope.path = '';	//	'http://mzereba.rww.io/storage/contacts/';
+    //$scope.path = 'http://essam.crosscloud.qcri.org/storage/contacts/';
     $scope.prefix = "vcard_";
     
     var providerURI = '//linkeddata.github.io/signup/index.html?ref=';
@@ -32,13 +32,11 @@ module.controller('ContactController', function ($scope, $http, $sce) {
     	 $scope.authenticationModal = true;	 
     };
     
-    /* Functions for modal management */
-    
     $scope.add = function() {
     	 $scope.modalTitle = "New Contact";
     	 $scope.editContactModal = true;
     };
-    
+       
     $scope.edit = function(id) {
     	$scope.modalTitle = "Edit Contact";
     	$scope.editContactModal = true;
@@ -48,12 +46,12 @@ module.controller('ContactController', function ($scope, $http, $sce) {
     $scope.save = function(newcontact) {
     	if (newcontact.id == null) {
             //if this is new contact, add it in contacts array
-            newcontact.id = $scope.contacts.length;
+            newcontact.id = new Date().getTime(); // Generate unique id
             $scope.insertContact(newcontact);
         } else {
             //for existing contact, find this contact using id
             //and update it.
-            for (i in newcontact) {
+            for (i in $scope.contacts) {
                 if ($scope.contacts[i].id == newcontact.id) {
                 	$scope.insertContact(newcontact);
                 }
@@ -92,8 +90,12 @@ module.controller('ContactController', function ($scope, $http, $sce) {
     	    }).
     	    success(function(data, status, headers) {
     	      if (status == 200) {
-    	        // remove resource from the view
-    	        $scope.removeResource(id, contactUri);
+    	    	console.log('Success', 'Deleted '+contactUri);
+    	    	// remove resource from the view
+    	        //$scope.removeContact(id, contactUri);
+    	        // Update view
+                $scope.contacts.length = 0;
+                $scope.load();
     	      }
     	    }).
     	    error(function(data, status) {
@@ -108,13 +110,12 @@ module.controller('ContactController', function ($scope, $http, $sce) {
     	      }
     	});
     	
-        if ($scope.newcontact.id == id) $scope.newcontact = {};
+        //if ($scope.newcontact.id == id) $scope.newcontact = {};
     };
     
     // Listing contact resources
     $scope.load = function () {
-    	//$scope.getContactsList();
-    	var g = $rdf.graph();
+		var g = $rdf.graph();
 	    var f = $rdf.fetcher(g);
 	    
 	    f.nowOrWhenFetched($scope.path + '*',undefined,function(){	
@@ -123,17 +124,18 @@ module.controller('ContactController', function ($scope, $http, $sce) {
 			var LDP = $rdf.Namespace('http://www.w3.org/ns/ldp#');
 			//var myOntology = $rdf.Namespace('http://user.pds.org/ontology/'); 
 			var VCARD = $rdf.Namespace('http://www.w3.org/2006/vcard/ns#');
-			
+	
 			var evs = g.statementsMatching(undefined, RDF('type'), VCARD('Individual'));
 			if (evs != undefined) {
-			    for (var e in evs) {
+				for (var e in evs) {
 					var id = evs[e]['subject']['value'];
+					var s = id.split("_"); 
 					var fullname = g.anyStatementMatching(evs[e]['subject'], VCARD('fn'))['object']['value'];
 					var email = g.anyStatementMatching(evs[e]['subject'], VCARD('hasEmail'))['object']['value'];
 					var phone = g.anyStatementMatching(evs[e]['subject'], VCARD('hasTelephone'))['object']['value'];
-		
+					
 					var contact = {
-					    id: id.slice(id.length-1),
+					    id: s[1],
 					    fullname: fullname,
 					    email: email,
 					    phone: phone 
@@ -147,11 +149,11 @@ module.controller('ContactController', function ($scope, $http, $sce) {
     
     // Function to insert or update a contact resource
     $scope.insertContact = function (contact) {
-	    var uri = $scope.path + $scope.prefix + contact.id;
-        var resource = $scope.composeRDFResource(contact, uri);
+	    var contactUri = $scope.path + $scope.prefix + contact.id;
+        var resource = $scope.composeRDFResource(contact, contactUri);
         $http({
           method: 'PUT', 
-          url: uri,
+          url: contactUri,
           data: resource,
           headers: {
             'Content-Type': 'text/turtle',
@@ -162,6 +164,9 @@ module.controller('ContactController', function ($scope, $http, $sce) {
         success(function(data, status, headers) {
           if (status == 200 || status == 201) {
             console.log('Success: Resource created.');
+            // Update view
+            $scope.contacts.length = 0;
+            $scope.load();
           }
         }).
         error(function(data, status) {
@@ -173,9 +178,6 @@ module.controller('ContactController', function ($scope, $http, $sce) {
             console.log('Failed '+ status + data);
           }
         });
-        
-        // Listing
-        $scope.load();
     };
     
     
@@ -211,7 +213,7 @@ module.controller('ContactController', function ($scope, $http, $sce) {
     $scope.composeRDFResource = function (contact, uri) {
         var rdf =   "<" + uri + ">\n" +
                 "a <http://www.w3.org/2000/01/rdf-schema#Resource>, <http://www.w3.org/2006/vcard/ns#Individual> ;\n" +
-                "<http://www.w3.org/2006/vcard/ns#fn> \"" + contact.name + "\" ;\n" +
+                "<http://www.w3.org/2006/vcard/ns#fn> \"" + contact.fullname + "\" ;\n" +
                 "<http://www.w3.org/2006/vcard/ns#hasEmail> <mailto:" + contact.email + "> ;\n" + 
                 "<http://www.w3.org/2006/vcard/ns#hasTelephone> <tel:" + contact.phone + "> .\n";
         return rdf;
@@ -237,7 +239,7 @@ module.controller('ContactController', function ($scope, $http, $sce) {
         if (e.data.slice(0,5) == 'User:') {          
             $scope.authenticate(e.data.slice(5, e.data.length));
             $scope.user = e.data.slice(5);
-            $scope.path1 = $scope.user.slice(0, $scope.user.length-15) + 'storage/contacts/';
+            $scope.path = $scope.user.slice(0, $scope.user.length-15) + 'storage/contacts/';
             //Fetch user data after login
             $scope.load();
         }
